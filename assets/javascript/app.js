e84bbbec22513b26898894e4d2c6d904b2723d77
Kayleighs <47003302+kayleighs@users.noreply.github.com>
2019-05-24 23:55:36 -0400

    some css added and gifs all in line

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,38 +8,46 @@ function renderButtons() {
         a.attr("data-name", topics[i]);
         a.text(topics[i]);
         $("#buttons-view").append(a);
+
     }
+    $(".topic").on("click", function () {
+        $(".content").empty();
+        console.log("clicked")
+        var searchTerm = $(this).attr("data-name");
+        var key = "peKXuRg486cY3D1jNy2naHJwSQq57NzQ"
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + key + "&limit=10"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                console.log(queryURL);
+                console.log(response);
+                var results = response.data;
+                for (var i = 0; i < results.length; i += 1) {
+                    var topicSpan = $("<span>");
+                    topicSpan.addClass("topic-span")
+                    var p = $("<p>").text("Rating: " + results[i].rating);
+                    var topicImage = $("<img>");
+                    topicImage.attr("src", results[i].images.fixed_height.url);
+                    topicSpan.append(p);
+                    topicSpan.append(topicImage);
+                    $(".content").prepend(topicSpan);
+                }
+            });
+    });
+
 }
 
+console.log(topics)
 $("#add-topic").on("click", function(event){
     event.preventDefault();
     var topic = $("#input-new").val().trim();
     topics.push(topic);
     renderButtons();
+    console.log(topics)
 });
-renderButtons();
-$("button").on("click",function(){
-    console.log("clicked")
-    var searchTerm = $(this).attr("data-name");
-    var key = "peKXuRg486cY3D1jNy2naHJwSQq57NzQ"
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ searchTerm + "&api_key=" + key + "&limit=10"
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    .then(function(response) {
-        console.log(queryURL);
-        console.log(response);
-        var results = response.data;
-        for (var i =0; i <results.length; i +=1){
-            var topicDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[i].rating);
-            var topicImage = $("<img>");
-            topicImage.attr("src", results[i].images.fixed_height.url);
-            topicDiv.append(p);
-            topicDiv.append(topicImage);
-            $(".content").prepend(topicDiv);
-        }
-    });
-});
+
+renderButtons();
