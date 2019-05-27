9158cbc0dc41254cbc2e67210051b112e70af9e8
Kayleighs <47003302+kayleighs@users.noreply.github.com>
2019-05-27 01:49:22 -0400

    working favorites and titles added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,11 +13,11 @@ function renderButtons() {
         $("#buttons-view").append(a);
     }
     $(".topic").on("click", function () {
-        $(".content").empty();
+   //     $(".content").empty();
         console.log("clicked")
         var searchTerm = $(this).attr("data-name");
         var key = "peKXuRg486cY3D1jNy2naHJwSQq57NzQ"
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + key + "&limit=10"
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + key + "&limit=10" + "&offset=0"
 
         $.ajax({
             url: queryURL,
@@ -34,6 +34,10 @@ function renderButtons() {
                     var ratingP = $("<p>").text("Rating: " + results[i].rating.toUpperCase());
                     ratingP.addClass("rating")
 
+                    var favorites = $("<button>").text("Save")
+                    favorites.addClass("save-gif")
+                    favorites.attr("url", results[i].images.fixed_height.url)
+
                     var topicImage = $("<img>")
                     var stillGif = results[i].images.fixed_height_still.url
                     var animatedGif = results[i].images.fixed_height.url
@@ -42,8 +46,11 @@ function renderButtons() {
                     topicImage.attr({ "data-animate": animatedGif, "data-still": stillGif });
                     topicImage.attr({ "src": stillGif, "data-state": "still" });
 
+                    
                     topicSpan.append(ratingP);
+                    topicSpan.append(favorites)
                     topicSpan.append(topicImage);
+
                     $(".content").prepend(topicSpan);
                 }
             });
@@ -62,13 +69,69 @@ $(document).on("click", ".gif", function () {
     }
 });
 
+//add buttons for new topics
 console.log(topics)
 $("#add-topic").on("click", function(event){
     event.preventDefault();
-    var topic = $("#input-new").val().trim();
+    var topic = $(this.results[i].images).val().trim();
     topics.push(topic);
     renderButtons();
     console.log(topics)
 });
 
+//saving to favorites and local storage
+
+function renderFavs(list) {
+    $("#favorites").empty(); 
+
+    // render  gif to the page
+    for (var i = 0; i < list.length; i++) {
+
+        var favSpan = $("<span>");
+        favSpan.addClass("topic-span")
+        var favItem = $("<img>");
+        favItem.attr("src", list[i]);
+
+        var deleteFav = $("<button>");
+
+        deleteFav.attr("data-to-do", i);
+        deleteFav.addClass("remove");
+        deleteFav.text("Remove ✘");
+
+
+  //      favItem = favItem.prepend(deleteFav);
+        favSpan.append(deleteFav)
+        $(favSpan).append(favItem)
+        $("#favorites").append(favSpan);
+    }
+}
+
+//saves item on click
+$(document).on("click", ".save-gif", function (event) {
+    console.log("save button clicked")
+    event.preventDefault();
+    var favGif = $(this).attr("url")
+    console.log(favGif)
+    list.push(favGif)
+    renderFavs(list)
+    localStorage.setItem("favGif", JSON.stringify(list));
+ //   $(".save-gif").val("");
+});
+//removes item from favs on click
+$(document).on("click", ".remove", function () {
+    var removeFav = $(this).attr("data-to-do");
+    list.splice(removeFav, 1);
+    renderFavs(list);
+    localStorage.setItem("favGif", JSON.stringify(list));
+});
+
+var list = JSON.parse(localStorage.getItem("favGif"));
+if (!Array.isArray(list)) {
+    list = [];
+}
+
+renderFavs(list);
+
+
+//render buttons function
 renderButtons();
